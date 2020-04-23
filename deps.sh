55fd61e3f25ab745da8d0a1f80187b25cf26764b
Yeolar <yeolar@qq.com>
2020-04-23 15:41:16 +0800

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -2,12 +2,12 @@
 DEPS=(
 )
 
-BUILDER_VER=1.2
+BUILDER_VER=1.3
 
 mkdir -p deps && cd deps
 
 if [ ! -d dep-builder-${BUILDER_VER} ]; then
-    rm -f dep-builder*
+    rm -rf dep-builder*
     wget https://github.com/Yeolar/dep-builder/archive/v${BUILDER_VER}.tar.gz -O dep-builder.tar.gz
     tar xzf dep-builder.tar.gz
 fi
