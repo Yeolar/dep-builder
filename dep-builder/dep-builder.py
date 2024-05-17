a8aa49df67b109d1ab4e221917edd8ee68f5923c
Y <yeolar@qq.com>
2024-05-17 22:26:34 +0800

    Update dep-builder.py

--- dep-builder/dep-builder.py
+++ dep-builder/dep-builder.py
@@ -1,125 +1,80 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2020 Yeolar
+# Copyright 2024 Yeolar
 #
 
 import multiprocessing
 import os
-import subprocess
 import sys
 
+BUILD = False
+REFRESH_SH = True
 
-def _wrap_with(code):
-    def inner(text, bold=False):
-        c = code
-        if bold:
-            c = '1;%s' % c
-        return '\033[%sm%s\033[0m' % (c, text)
-    return inner
+SHELL_TPL = """#!/bin/sh
 
-red = _wrap_with('31')
-green = _wrap_with('32')
-yellow = _wrap_with('33')
-blue = _wrap_with('34')
-magenta = _wrap_with('35')
-cyan = _wrap_with('36')
-white = _wrap_with('37')
+if [ ! -f %(file)s ]; then
+    wget %(url)s
+fi
+rm -rf %(key)s
+%(extract)s
+cd %(key)s
+%(cmds)s
+"""
 
+class Dependency(object):
 
-def run(*cmd):
-    cmdstr = ' '.join(cmd)
-    print cyan(cmdstr)
-    return subprocess.call(cmdstr, shell=True)
+    EXTRACT = dict((
+        ('', ''),
+        ('.zip', 'unzip %(file)s'),
+        ('.tar.gz', 'tar xzf %(file)s'),
+    ))
 
+    def __init__(self, first_line):
+        self.key, _, url = first_line.partition(':')
+        self.url = url.strip()
+        self.file = self.url.split('/')[-1]
+        base, ext = os.path.splitext(self.file)
+        self.sh = base + '.sh'
+        self.extract = self.EXTRACT[ext]
+        self.dest = os.path.join(os.getcwd(), 'usr', 'local')
+        self._cmds = []
 
-class cd(object):
+    def __str__(self):
+        return self.key + ': ' + self.url
 
-    def __init__(self, path):
-        if not os.path.exists(path):
-            os.makedirs(path, 0755)
-        self.path = path
-        self.oldpath = os.getcwd()
-
-    def __enter__(self):
-        os.chdir(self.path)
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        os.chdir(self.oldpath)
-
-
-TYPE_GIT = 0
-TYPE_ZIP = 1
-TYPE_GZ = 2
-
-def get_type(url):
-    if url.lower().endswith('.tar.gz'):
-        return TYPE_GZ
-    if url.lower().endswith('.zip'):
-        return TYPE_ZIP
-    return TYPE_GIT
-
-
-def read_deps(f, *args):
-    deps = []
-    if f != '-':
-        with open(f) as fp:
-            args = fp.readlines()
-    for arg in args:
-        s = arg.strip()
-        if not s.startswith('#'):
-            url, _, commit = s.partition('@')
-            type = get_type(url)
-            commit, _, build = commit.partition(':')
-            deps.append({
-                'raw': s,
-                'type': type,
-                'url': url,
-                'root': url.split('/')[-1],
-                'commit': commit,
-                'build': build or 'cmake ..'
-            })
-    return deps
-
-
-def build_dep(root, commit, build, jobs=multiprocessing.cpu_count() * 2):
-    path = os.path.join(os.getcwd(), 'usr', 'local')
-    with cd(root):
-        if commit is not None:
-            if commit:
-                run('git checkout', commit)
-            run('git pull')
-        if build.startswith('cmake'):
-            with cd('_build'):
-                build = 'cmake -DCMAKE_PREFIX_PATH=' + path + build[5:]
-                run(build)
-                run('make -j%d' % jobs)
-                run('make DESTDIR=../.. install')
+    def add_command(self, cmd):
+        cmd = cmd.strip()
+        if cmd.startswith('cmake'):
+            self._cmds.append('mkdir _build && cd _build')
+            self._cmds.append('cmake -DCMAKE_PREFIX_PATH=%(dest)s' + cmd[5:])
         else:
-            run(build)
-            run('make -j%d' % jobs)
-            run('make DESTDIR=.. install')
+            self._cmds.append(cmd)
 
+    def generate(self):
+        self._cmds.append('make -j%d' % multiprocessing.cpu_count())
+        self._cmds.append('make DESTDIR=%(dest)s install')
+        self.cmds = '\n'.join(self._cmds)
+        return SHELL_TPL % self.__dict__ % self.__dict__
 
-if __name__ == '__main__':
-    deps = read_deps(*sys.argv[1:])
-    for dep in deps:
-        if not os.path.exists(dep['root']):
-            print 'fetch', white(dep['raw'], True)
-            if dep['type'] == TYPE_GIT:
-                run('git', 'clone', dep['url'])
-            else:
-                run('wget', dep['url'])
-    for dep in deps:
-        if dep['type'] != TYPE_GIT and not os.path.exists(dep['commit']):
-            print 'unzip', white(dep['raw'], True)
-            if dep['type'] == TYPE_GZ:
-                run('tar', 'xzf', dep['root'])
-            if dep['type'] == TYPE_ZIP:
-                run('unzip', dep['root'])
-    for dep in deps:
-        print 'build', white(dep['raw'], True)
-        if dep['type'] == TYPE_GIT:
-            build_dep(dep['root'], dep['commit'], dep['build'])
+deps = []
+
+with open(sys.argv[1]) as fp:
+    lines = [line.rstrip() for line in fp.readlines()]
+
+for line in lines:
+    if line.startswith('#'):
+        continue
+    if len(line) > 0:
+        if line[0] != ' ':
+            deps.append(Dependency(line))
         else:
-            build_dep(dep['commit'], None, dep['build'])
+            deps[-1].add_command(line)
+
+for dep in deps:
+    if not os.path.exists(dep.sh) or REFRESH_SH:
+        with open(dep.sh, 'w') as fp:
+            fp.write(dep.generate())
+if BUILD:
+    for dep in deps:
+        os.system('sh ' + dep.sh)
