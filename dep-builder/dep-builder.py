39b09dbe93cc2fcd7633fac2bc21277a2b6d2fe9
yeolar <yeolar@qq.com>
2024-05-18 17:12:56 +0800

    udpate

--- dep-builder/dep-builder.py
+++ dep-builder/dep-builder.py
@@ -4,14 +4,15 @@
 # Copyright 2024 Yeolar
 #
 
+import argparse
 import multiprocessing
 import os
 import sys
 
-BUILD = False
-REFRESH_SH = True
+ROOT = os.path.dirname(os.path.abspath(__file__))
+CPU_COUNT = multiprocessing.cpu_count()
 
-SHELL_TPL = """#!/bin/sh
+SHELL_TPL = """#!/bin/sh -x
 
 if [ ! -f %(file)s ]; then
     wget %(url)s
@@ -32,12 +33,11 @@ class Dependency(object):
 
     def __init__(self, first_line):
         self.key, _, url = first_line.partition(':')
+        self.sh = self.key + '.sh'
         self.url = url.strip()
         self.file = self.url.split('/')[-1]
-        base, ext = os.path.splitext(self.file)
-        self.sh = base + '.sh'
-        self.extract = self.EXTRACT[ext]
-        self.dest = os.path.join(os.getcwd(), 'usr', 'local')
+        self.extract = self.EXTRACT[os.path.splitext(self.file)[1]]
+        self.root = ROOT
         self._cmds = []
 
     def __str__(self):
@@ -45,36 +45,55 @@ class Dependency(object):
 
     def add_command(self, cmd):
         cmd = cmd.strip()
-        if cmd.startswith('cmake'):
+        if cmd.startswith('CMAKE'):
             self._cmds.append('mkdir _build && cd _build')
-            self._cmds.append('cmake -DCMAKE_PREFIX_PATH=%(dest)s' + cmd[5:])
+            self._cmds.append('cmake -DCMAKE_PREFIX_PATH=%(root)s/usr/local'
+                              + cmd[len('CMAKE'):])
+        elif cmd.startswith('MAKE_WITH_INSTALL'):
+            self._cmds.append('make -j%d' % CPU_COUNT
+                              + cmd[len('MAKE_WITH_INSTALL'):])
+            self._cmds.append('make DESTDIR=%(root)s install')
+        elif cmd.startswith('MAKE'):
+            self._cmds.append('make -j%d' % CPU_COUNT + cmd[len('MAKE'):])
         else:
             self._cmds.append(cmd)
 
     def generate(self):
-        self._cmds.append('make -j%d' % multiprocessing.cpu_count())
-        self._cmds.append('make DESTDIR=%(dest)s install')
         self.cmds = '\n'.join(self._cmds)
         return SHELL_TPL % self.__dict__ % self.__dict__
 
-deps = []
 
-with open(sys.argv[1]) as fp:
-    lines = [line.rstrip() for line in fp.readlines()]
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser(description='C++ dep builder.')
+    ap.add_argument('conf', help='dep config file')
+    ap.add_argument('--targets', action='store', dest='targets',
+                    help='build targets')
+    ap.add_argument('--refresh-only', action='store_true', dest='refresh_only',
+                    help='refresh sh only')
+    args = ap.parse_args()
 
-for line in lines:
-    if line.startswith('#'):
-        continue
-    if len(line) > 0:
-        if line[0] != ' ':
-            deps.append(Dependency(line))
-        else:
-            deps[-1].add_command(line)
+    deps = []
 
-for dep in deps:
-    if not os.path.exists(dep.sh) or REFRESH_SH:
-        with open(dep.sh, 'w') as fp:
-            fp.write(dep.generate())
-if BUILD:
+    with open(args.conf) as fp:
+        lines = [line.rstrip() for line in fp.readlines()]
+
+    for line in lines:
+        if line.startswith('#'):
+            continue
+        if len(line) > 0:
+            if line[0] != ' ':
+                deps.append(Dependency(line))
+            else:
+                deps[-1].add_command(line)
+
+    targets = args.targets and args.targets.split(',') or []
+
     for dep in deps:
-        os.system('sh ' + dep.sh)
+        if not targets or dep.key in targets:
+            with open(os.path.join(dep.root, dep.sh), 'w') as fp:
+                fp.write(dep.generate())
+
+    if not args.refresh_only:
+        for dep in deps:
+            if not targets or dep.key in targets:
+                os.system('cd %(root)s && sh -x %(sh)s' % dep.__dict__)
